c38c215e15015d4a706fa65a41a955b94b8fbb6c
Fauzi Achmad <fauziachmadk@users.noreply.github.com>
2025-05-28 10:00:07 +0700

    Commit by Fauzi

--- app/Models/Peminjamans.php
+++ app/Models/Peminjamans.php
@@ -4,7 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Peminjamans extends Model
+class Peminjaman extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 
+        'buku_id', 
+        'tanggal_pinjam', 
+        'tanggal_kembali', 
+        'status'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function buku() {
+        return $this->belongsTo(Buku::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,28 +11,28 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'alamat',
+        'no_telepon',
+        'role',
     ];
 
-    /**
+        /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
+
+        /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
@@ -43,5 +43,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    // User.php
+    public function favoritBuku() {
+        return $this->belongsToMany(Buku::class, 'favorites')->withPivot('catatan_opsional')->withTimestamps();
+    }
+
+    // Buku.php
+    public function favoritOleh() {
+        return $this->belongsToMany(User::class, 'favorites')->withPivot('catatan_opsional')->withTimestamps();
     }
 }
