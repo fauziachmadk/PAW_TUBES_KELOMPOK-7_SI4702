c82abecf84fdc6229b0a49ffc380b8e764c5295d
Nadilla Ismanullah <nadilla1412@gmail.com>
2025-05-31 19:51:13 +0700

    commit by Nadilla

---
+++ resources/views/anggotas/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1>Tambah Anggota</h1>
+    <form action="{{ route('anggotas.store') }}" method="POST">
+        @csrf
+        @include('admin.anggotas.form')
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
+</div>
+@endsection
