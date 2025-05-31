963dcbac8132a65267c21efb9692998b018b906f
michaelasl <brigitaselva10@gmail.com>
2025-05-31 17:10:03 +0700

    commit by selva

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,5 +13,33 @@ class BookController extends Controller
         $books = Buku::all();
         return view('admin.books.index', compact('books'));
     }
+    public function create()
+    {
+        return view('admin.books.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'judul' => 'required',
+            'penulis' => 'required',
+            'penerbit' => 'required',
+            'tahun_terbit' => 'required|digits:4|integer',
+            'genre_buku' => 'required|string|max:100',
+            'gambar_buku' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('gambar_buku')) {
+            if ($buku->gambar_buku) {
+                Storage::disk('public')->delete($buku->gambar_buku);
+            }
+            $validated['gambar_buku'] = $request->file('gambar_buku')->store('covers', 'public');
+        }
+
+        $buku->update($validated);
+        return redirect()->route('books.index')->with('success', 'Buku berhasil diperbarui!');
+        
+    }
+
     
 }
