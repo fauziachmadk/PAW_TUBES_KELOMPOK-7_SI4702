44bf78899732aa1848f4433ce344a44959f2cf1d
Fauzi Achmad <fauziachmadk@users.noreply.github.com>
2025-06-01 22:44:12 +0700

    Main Branch

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -31,6 +31,33 @@ class BookController extends Controller
         ]);
 
         if ($request->hasFile('gambar_buku')) {
+            $validated['gambar_buku'] = $request->file('gambar_buku')->store('covers', 'public');
+        }
+
+        Buku::create($validated);
+        return redirect()->route('books.index')->with('success', 'Buku berhasil ditambahkan!');
+    }
+
+        public function edit($id)
+    {
+        $buku = Buku::findOrFail($id);
+        return view('admin.books.edit', compact('buku'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $buku = Buku::findOrFail($id);
+        
+        $validated = $request->validate([
+            'judul' => 'required',
+            'penulis' => 'required',
+            'penerbit' => 'required',
+            'tahun_terbit' => 'required|digits:4|integer',
+            'genre_buku' => 'required|string|max:100',
+            'gambar_buku' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('gambar_buku')) {
             if ($buku->gambar_buku) {
                 Storage::disk('public')->delete($buku->gambar_buku);
             }
@@ -40,7 +67,7 @@ class BookController extends Controller
         $buku->update($validated);
         return redirect()->route('books.index')->with('success', 'Buku berhasil diperbarui!');
     }
-
+    
     public function destroy($id)
     {
         $buku = Buku::findOrFail($id);
